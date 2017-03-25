268fcf7a382ad7612d7087b28b026ad3bb163e57
Maximilian Schwarzmüller <mblacky0@gmail.com>
2017-03-25 10:59:31 +0100

    starting setup

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,3 @@
-import '../css/main.scss';
-import { RandomGenerator } from './random-generator';
-
 const outputParagraph = document.querySelector('#outputParagraph');
 
 const outputRandomInt = () => {
